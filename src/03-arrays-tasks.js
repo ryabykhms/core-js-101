9288b903b666df9799053b88a76e6f9ea3c3836d
ryabykhms <ryabykhms@gmail.com>
2020-12-02 18:36:36 +0300

    feat: solve arrays 18: returns the number of positive numbers from specified array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -295,8 +295,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((item) => Number.isInteger(item) && item > 0).length;
 }
 
 /**
