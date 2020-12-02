85f99a84797c1b36eeb8682c1d5d188c26229667
ryabykhms <ryabykhms@gmail.com>
2020-12-02 16:04:09 +0300

    feat: solve arrays 12: returns CSV represebtation of two-dimentional numeric array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -194,8 +194,8 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.reduce((acc, cur) => `${acc}\n${cur.join()}`, '').slice(1);
 }
 
 /**
