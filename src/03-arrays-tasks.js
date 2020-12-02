c400001dadd2ba88fb962ace0c92bca03eda80eb
ryabykhms <ryabykhms@gmail.com>
2020-12-02 18:40:08 +0300

    feat: solve arrays 19: sorts digit names

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -312,8 +312,21 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbers = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  return arr.sort((a, b) => numbers[a] - numbers[b]);
 }
 
 /**
