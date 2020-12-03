eec22328c53bc1a1f3f11d50412a604f2084be1a
ryabykhms <ryabykhms@gmail.com>
2020-12-03 09:08:44 +0300

    feat: solve arrays 30: swaps the head and tail of the specified array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -591,8 +591,17 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const partLength = Math.floor(arr.length / 2);
+  const head = arr.slice(0, partLength);
+  const tail = arr.slice(-partLength);
+  if (arr.length % 2) {
+    tail.push(arr[partLength]);
+  }
+  return [...tail, ...head];
 }
 
 module.exports = {
