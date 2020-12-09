98696c97255a50dbb89ff3d9edbf3f9a4366e10e
ryabykhms <ryabykhms@gmail.com>
2020-12-09 14:27:42 +0300

    feat: solve c-n-l task 14: toNaryString

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -383,8 +383,16 @@ function isBracketsBalanced(/* str */) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  let number = num;
+  let result = '';
+
+  while (number) {
+    result = `${number % n}${result}`;
+    number = Math.trunc(number / n);
+  }
+
+  return result;
 }
 
 /**
