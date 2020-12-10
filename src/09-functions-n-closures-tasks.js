9ac7470d0ac3f624c6ec74341a52d2c626813b21
ryabykhms <ryabykhms@gmail.com>
2020-12-10 10:36:55 +0300

    feat: solve f-n-c task 4: memoize

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -94,8 +94,9 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const result = func();
+  return () => result;
 }
 
 /**
